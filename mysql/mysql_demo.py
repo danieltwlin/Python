0d6c95e378b5274bf24164880fedc500ac04caea
daniel <daniel.lin.gr@gmail.com>
2019-11-08 16:36:49 +0800

    Add files via upload
    demo basic operation

---
+++ mysql/mysql_demo.py
@@ -0,0 +1,42 @@
+''' mysql example '''
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="192.168.0.233",
+  user="daniel",
+  passwd="daniel",
+  database="RD_Monitor"
+)
+
+print(mydb)
+mycursor = mydb.cursor()
+
+# query
+if(0):
+
+	mycursor.execute("SELECT * FROM AppStoreVersion")
+	myresult = mycursor.fetchall()
+
+	for x in myresult:
+		print(x)
+
+# Insert
+if(1):
+	sql = "INSERT INTO  AppStoreVersion(Product,Status,Play_Version,Baidu_Version ) VALUES (%s, %s, %s, %s)"
+	val = ("myApp",'OK',"1.2.3.00","1.2.3.00")
+	mycursor.execute(sql, val)
+	
+	mydb.commit()
+	
+	print(mycursor.rowcount, "record inserted.")
+		
+# update
+if(0):		
+	sql = "UPDATE test_table SET version = '1.2.3' WHERE id = '1'"
+
+	mycursor.execute(sql)
+
+	mydb.commit()
+
+	print(mycursor.rowcount, "record(s) affected")	
