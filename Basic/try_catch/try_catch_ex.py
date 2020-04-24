35dbc74c56d8ee9a5e3d5690e02544cd10cc6d62
daniel <daniel.lin.gr@gmail.com>
2020-04-24 11:58:00 +0800

    Update try_catch_ex.py

--- Basic/try_catch/try_catch_ex.py
+++ Basic/try_catch/try_catch_ex.py
@@ -1,11 +1,21 @@
+# 直接拋出異常，中止程式
+'''
+raise语句
+主动抛出异常。
+格式：
+主动抛出异常终止程序
+raise 异常名称(‘异常描述’)
+'''
+
+raise RuntimeError('testError')
+
+
+
+
 # 人工產生異常
-
 try:
-
-	raise BaseException("錯誤原因")   # raise ErrorType(" Error Message ")
-  
+	raise BaseException("錯誤原因")   # raise ErrorType(" Error Message ")  
 except(Exception,ex):
-
 	print(Exception,":",ex)
 
 
