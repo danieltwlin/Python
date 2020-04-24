947cf3581e09fd7e1986f4e8b8e698ff3762fcfb
daniel <daniel.lin.gr@gmail.com>
2020-04-24 11:28:19 +0800

    Create try_catch_ex.py

---
+++ Basic/try_catch/try_catch_ex.py
@@ -0,0 +1,15 @@
+# 人工產生異常
+
+try:
+
+	raise BaseException("錯誤原因")   # raise ErrorType(" Error Message ")
+  
+except Exception,ex:
+
+	print(Exception,":",ex)
+
+
+# 斷言
+self.assertEqual(ret,'OK')
+
+
