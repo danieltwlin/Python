cbfe401e483c6c1cefbb1851ca5f802f62faf752
daniel <67772752+danieltwlin@users.noreply.github.com>
2020-07-29 17:51:01 +0800

    Update calculate_time.py

--- Operating System/calculate_time.py
+++ Operating System/calculate_time.py
@@ -1,1 +1,26 @@
-test
+import datetime,time
+import os
+
+# Start Time
+starttime = datetime.datetime.now()
+
+#long running
+#time.sleep(2)
+
+
+# code
+os.system('sh nasrsync.sh')
+
+# data
+#os.system('sh nasrsync2.sh')
+
+
+# Get Total Time
+endtime = datetime.datetime.now()
+print((endtime - starttime).seconds)
+
+# Time Format
+seconds = (endtime - starttime).seconds
+m, s = divmod(seconds, 60)
+h, m = divmod(m, 60)
+print("%02d:%02d:%02d" % (h, m, s))
