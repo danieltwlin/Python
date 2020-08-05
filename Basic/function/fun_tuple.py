25ed6b17f3da7fd1d16aae0ac3b99ffd80e12144
daniel <67772752+danieltwlin@users.noreply.github.com>
2020-08-05 14:17:21 +0800

    Update fun_tuple.py

--- Basic/function/fun_tuple.py
+++ Basic/function/fun_tuple.py
@@ -2,11 +2,15 @@ def profile():
     name = "Danny"
     age = 30
     return name, age
-print (profile())
+
+# call function
+print(profile())
+
+# return tuple value
 name1,age1 = profile() #直接profile()函式賦值給name1，age1
 
-print (name1)
-print (age1)
+print(name1)
+print(age1)
 
 
 '''
