659e2a5a5cea7758be3fd094229b057590503084
daniel <67772752+danieltwlin@users.noreply.github.com>
2020-09-01 14:56:11 +0800

    Update check_element.py

--- selenium/check_element.py
+++ selenium/check_element.py
@@ -1,3 +1,15 @@
 driver.find_element_by_xpath("//*[contains(text(), 'hello ')]").is_displayed()
 
 driver.find_element_by_xpath(" xxxx").is_displayed() 
+
+
+
+# 檢查是否刪除學生是否成功		
+html = driver.page_source		
+#print(html)
+
+keyword = 'Baab'
+if(keyword in html ):
+	print('刪除學生失敗~!')
+else:
+	print('成功刪除~!')
