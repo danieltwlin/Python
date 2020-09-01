bcab29bdb17972c4c29388491ce48e9d2f9c5e5b
daniel <67772752+danieltwlin@users.noreply.github.com>
2020-09-01 14:57:59 +0800

    Update check_element.py

--- selenium/check_element.py
+++ selenium/check_element.py
@@ -1,10 +1,8 @@
+# 檢查網頁文字或元件是否存在
 driver.find_element_by_xpath("//*[contains(text(), 'hello ')]").is_displayed()
-
 driver.find_element_by_xpath(" xxxx").is_displayed() 
 
-
-
-# 檢查是否刪除學生是否成功		
+# 由page_source 檢查是文字是否存在
 html = driver.page_source		
 #print(html)
 
