319cc22a8d3fb090a9b9e554134638abb0c39eb5
daniel <67772752+danieltwlin@users.noreply.github.com>
2021-02-01 17:04:07 +0800

    Update paramiko_demo.py

--- Application&Tool/交互式的指令操作/paramiko_demo.py
+++ Application&Tool/交互式的指令操作/paramiko_demo.py
@@ -13,7 +13,9 @@ ssh.connect(hostname='192.168.1.105', port=22, username='root', password='123456
 stdin, stdout, stderr = ssh.exec_command('df -h ')  # stdout 为正确输出，stderr为错误输出，同时是有1个变量有值
 
 # 打印执行结果
-print(stdout.read().decode('utf-8'))
+print(stdout.read().decode('utf-8'))    
+
+# print(stdout.read().decode('utf-8', 'ignore'))
 
 # 关闭SSHClient
 ssh.close()
