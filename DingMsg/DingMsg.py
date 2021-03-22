be4bb97cd2fffe8c1b37416c1d12376887b55343
daniel <67772752+danieltwlin@users.noreply.github.com>
2021-03-22 17:54:17 +0800

    Update DingMsg.py

--- DingMsg/DingMsg.py
+++ DingMsg/DingMsg.py
@@ -3,13 +3,14 @@ from dingtalkchatbot.chatbot import DingtalkChatbot
 # Must install DingtalkChatbot 
 # pip install DingtalkChatbot
 
-#WebHook地址
-webhook = 'https://oapi.dingtalk.com/robot/send?access_token=943d6b80848c94cb473d1e11234567890'
+def DingMsg(msg):
+  #WebHook地址
+  webhook = 'https://oapi.dingtalk.com/robot/send?access_token=xxxx'
 
-# 初始化机器人小丁
-xiaoding = DingtalkChatbot(webhook)
+  # 初始化机器人小丁
+  xiaoding = DingtalkChatbot(webhook)
 
-#Text消息@所有人
-xiaoding.send_text(msg='我就是小丁，小丁就是我！', is_at_all=True)
+  #Text消息@所有人
+  xiaoding.send_text(msg, is_at_all=True)
 
 
