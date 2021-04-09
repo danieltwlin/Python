3b8d3bb2b41d81d79ac5049d3a25bc8d8cb7595d
daniel <67772752+danieltwlin@users.noreply.github.com>
2021-04-09 15:52:49 +0800

    Create monitor_adas_pod.py

---
+++ App/monitor_resque/monitor_adas_pod.py
@@ -0,0 +1,62 @@
+import os
+
+# execute command, and return the output
+def execCmd(cmd):
+	r = os.popen(cmd)
+	text = r.read()
+	r.close()
+	return text
+
+# get pod id
+def get_pod(cmd):
+
+	ret  = execCmd('kubectl get pods | grep adas')
+
+	print( ret)
+
+	lst1 = ret.split()
+	#print(lst1)
+
+	pod = lst1[0]
+	print(pod)
+
+	return pod
+
+# get pod process status
+def get_pod_process_status(cmd, key):
+
+	print(cmd)
+	#ret  = execCmd("kubectl exec podid '/opt/lampp/lampp status)")
+	ret = execCmd(cmd)
+	print( ret)
+
+	status =  False
+
+	#print(key)
+
+	if( key in ret):
+		status = True
+
+	return status
+
+
+'''	Check adas pod	'''
+if __name__ == '__main__':
+
+	cmd = 'kubectl get pods | grep adas'
+
+	pod = get_pod(cmd)
+
+	cmd = "kubectl exec "+ pod + " -- /opt/lampp/lampp status"
+	key = "Apache is running."
+	status = get_pod_process_status(cmd,key)
+
+	#status = False
+	if( status):
+		print('ADAS job is Ok!')
+	else:
+		alertMsg = 'ADAS job is Error!'
+		print(alertMsg)
+		raise RuntimeError(alertMsg)
+
+
