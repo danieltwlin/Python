86ea68d7320e8ddddeb2d9f4004c0d765d420680
daniel <67772752+danieltwlin@users.noreply.github.com>
2021-04-12 13:35:35 +0800

    Update with_write.py

--- FileSystem/with_write.py
+++ FileSystem/with_write.py
@@ -1,3 +1,11 @@
 # 用 with 寫入檔案
 with open('/Users/michael/test.txt', 'w') as f:
     f.write('Hello, world!')
+
+   
+    
+    
+# 設定編碼 
+with open(name, 'r', encoding='UTF-8') as f:
+    content = f.read()
+    print(content)
