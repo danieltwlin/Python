dc41422f8baa67037c13ebb8a47da616d1db0cba
daniel <67772752+danieltwlin@users.noreply.github.com>
2021-04-12 14:00:30 +0800

    Create log_to_file.py

---
+++ Operating System/log_to_file.py
@@ -0,0 +1,22 @@
+mport os,sys
+import datetime
+
+# write log to file
+def log_file(file,log):
+
+        # Get Time Now
+        x = datetime.datetime.now()
+        dt_now = str(x.year) + '/' + str(x.month).zfill(2) + '/' + str(x.day).zfill(2) + ' ' + str(x.hour).zfill(2) + ':'  +str(x.minute).zfill(2) + ':' +str(x.second).zfill(2)
+
+        # log Time, Msg
+        with open(file, 'a') as f:
+                #f.write('Hello, world!')
+                f.write(dt_now + '\n')
+                f.write(log + '\n')
+
+
+if __name__=='__main__':
+
+        file = 'logfile.log'
+        log = 'test1'
+        log_file(file,log)
