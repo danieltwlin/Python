49466db3708ac1bc16e9af09043168a31cb185fc
daniel <67772752+danieltwlin@users.noreply.github.com>
2021-04-12 15:28:59 +0800

    Update log_to_file.py

--- Operating System/log_to_file.py
+++ Operating System/log_to_file.py
@@ -6,6 +6,7 @@ def log_file(file,log,time_flag = False):
 
         # Get Time Now
         x = datetime.datetime.now()
+        #2021/04/12 15:26:23
         dt_now = str(x.year) + '/' + str(x.month).zfill(2) + '/' + str(x.day).zfill(2) + ' ' + str(x.hour).zfill(2) + ':'  +str(x.minute).zfill(2) + ':' +str(x.second).zfill(2)
 
         # log Time, Msg
