5d430582c457ca4ea300893c27f6b02bee00f80e
daniel <67772752+danieltwlin@users.noreply.github.com>
2021-04-12 15:28:20 +0800

    Update log_to_file.py

--- Operating System/log_to_file.py
+++ Operating System/log_to_file.py
@@ -1,23 +1,24 @@
-mport os,sys
+import os,sys
 import datetime
 
-# write log to file
-def log_file(file,log):
+
+def log_file(file,log,time_flag = False):
 
         # Get Time Now
-        x = datetime.datetime.now()  
-        # 2021/04/12 13:56:47
+        x = datetime.datetime.now()
         dt_now = str(x.year) + '/' + str(x.month).zfill(2) + '/' + str(x.day).zfill(2) + ' ' + str(x.hour).zfill(2) + ':'  +str(x.minute).zfill(2) + ':' +str(x.second).zfill(2)
 
         # log Time, Msg
         with open(file, 'a') as f:
-                #f.write('Hello, world!')
-                f.write(dt_now + '\n')
+
+                if(time_flag):
+                        f.write(dt_now + '\n')
+
                 f.write(log + '\n\n')
 
 
 if __name__=='__main__':
 
-        file = 'logfile.log'
+        file = 'testlog.log'
         log = 'test1'
-        log_file(file,log)
+        log_file(file,log,'True')
