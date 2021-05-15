7df37f6c18cf37d5495f4ff5c3df18ce0e2fa7c3
daniel <daniel.lin.gr@gmail.com>
2021-05-15 18:00:45 +0800

    Update sample_get.py

--- exam/2/sample_get.py
+++ exam/2/sample_get.py
@@ -1,9 +1,10 @@
 # -*- coding: utf8 -*-
 import os,sys
 import requests
+import pytest
 ''' Demo Simple Get '''
 
-def sample_get():
+def test_website1():
 
 	# url
 	url = "https://cathay-ds-test.s3-ap-northeast-1.amazonaws.com/user_info?userid=A123456789"
@@ -13,12 +14,11 @@ def sample_get():
 	# Assert Response Code is 200
 	assert response.status_code == 200
 
+def test_website2():
+
 	url = "https://cathay-ds-test.s3-ap-northeast-1.amazonaws.com/company_info?companyid=1"
 	print("url = " + url)
 	response = requests.get(url)
 	print("Response Code " + str(response.status_code))	
-    # Assert Response Code is 403
+	# Assert Response Code is 403
 	assert response.status_code == 403
-	
-if __name__ == "__main__":
-	sample_get()
