0aea0bb8e485c3e0fc49918921d54ccee139aafd
daniel <daniel.lin.gr@gmail.com>
2021-05-15 13:21:03 +0800

    Create sample_get.py

---
+++ Net Application/Restful API/sample_get.py
@@ -0,0 +1,19 @@
+import requests
+import json
+''' Demo Simple Get '''
+
+def main():
+
+	# newies tw
+	url = "https://www.google.com"
+
+	response = requests.get(url).json()
+	
+	print( response)
+  print( response.status_code)
+	
+	#assert "200" not in Response Code
+	assert response["code"] != "200"
+
+if __name__ == "__main__":
+	main()
