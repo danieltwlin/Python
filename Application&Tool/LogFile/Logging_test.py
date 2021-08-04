6c18611d559cca548e80c6fbf0a002be44e285af
daniel <67772752+danieltwlin@users.noreply.github.com>
2021-08-04 17:31:45 +0800

    Update Logging_test.py

--- Application&Tool/LogFile/Logging_test.py
+++ Application&Tool/LogFile/Logging_test.py
@@ -3,6 +3,7 @@ import logging
 FORMAT = '%(asctime)s %(message)s'
 logging.basicConfig(level=logging.DEBUG, filename='myLog.log',format=FORMAT)
 # logging.basicConfig(level=logging.DEBUG,format=FORMAT)  # 不要logfile，就不要設定 filename
+# logging.basicConfig(level=logging.CRITICAL,format=FORMAT)   # 嚴重 Error 才輸出
 
 logging.debug('debug message')
 logging.info('info message')
