d7574c5b9645a968dfd9fb4921def868fc7ffefd
daniel <67772752+danieltwlin@users.noreply.github.com>
2021-08-04 15:53:30 +0800

    Update Logging_test.py

--- Application&Tool/LogFile/Logging_test.py
+++ Application&Tool/LogFile/Logging_test.py
@@ -2,6 +2,7 @@ import logging
 
 FORMAT = '%(asctime)s %(message)s'
 logging.basicConfig(level=logging.DEBUG, filename='myLog.log',format=FORMAT)
+# logging.basicConfig(level=logging.DEBUG,format=FORMAT)  # 不要logfile，就不要設定 filename
 
 logging.debug('debug message')
 logging.info('info message')
