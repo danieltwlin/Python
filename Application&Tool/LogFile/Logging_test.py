bd8dc270d4882b5f33800c480c8b3ae5a5769159
daniel <67772752+danieltwlin@users.noreply.github.com>
2021-08-04 15:39:39 +0800

    Create Logging_test.py

---
+++ Application&Tool/LogFile/Logging_test.py
@@ -0,0 +1,10 @@
+import logging
+
+FORMAT = '%(asctime)s %(message)s'
+logging.basicConfig(level=logging.DEBUG, filename='myLog.log',format=FORMAT)
+
+logging.debug('debug message')
+logging.info('info message')
+logging.warning('warning message')
+logging.error('error message')
+logging.critical('critical message')
