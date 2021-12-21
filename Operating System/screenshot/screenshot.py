9cc276548b6fc1de57fd9097eb0f2a89680c9bcc
daniel <daniel.lin.gr@gmail.com>
2021-12-21 10:19:05 +0800

    Update screenshot.py

--- Operating System/screenshot/screenshot.py
+++ Operating System/screenshot/screenshot.py
@@ -5,8 +5,8 @@ myScreenshot.save(r'D:\screenshot_1.png')
 
 # 取得時間
 from datetime import datetime
-dt = datetime.now().strftime("%m-%d %H:%M")
+dt = datetime.now().strftime("%m-%d %H%M")
 print(dt)
 
 myScreenshot = pyautogui.screenshot()
-myScreenshot.save('D:\\'+ dt +'screenshot_1.png')
+myScreenshot.save('D:\\'+ dt +' screenshot_1.png')
